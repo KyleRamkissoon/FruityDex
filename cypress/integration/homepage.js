065554ae84479c5a1b10e76cec1d349cd9e94dd6
Kyle Ramkissoon <kyleramkissoon90@gmail.com>
2021-12-12 12:10:53 -0400

    Cypress Testing Further [WIP]

--- cypress/integration/homepage.js
+++ cypress/integration/homepage.js
@@ -1,23 +1,22 @@
 /* eslint-disable no-undef */
-describe('Example test', () => {
-  before(() => {
-    cy.signIn()
-  })
-
-  after(() => {
-    cy.clearLocalStorageSnapshot()
-    cy.clearLocalStorage()
-  })
-
-  beforeEach(() => {
-    cy.restoreLocalStorage()
-  })
-
-  afterEach(() => {
-    cy.saveLocalStorage()
-  })
-
-  it('should be logged in', () => {
+describe('Authenticator:', function () {
+  // Step 1: setup the application state
+  beforeEach(function () {
     cy.visit('/')
   })
+
+  describe('Sign In:', () => {
+    it('allows a user to signin', () => {
+      // Step 2: Take an action (Sign in)
+      cy.get(selectors.usernameInput).type('fruitdex3613@gmail.com')
+      cy.get(selectors.signInPasswordInput).type('password')
+      cy.get(selectors.signInSignInButton).contains('Sign In').click()
+    })
+  })
 })
+export const selectors = {
+  // Auth component classes
+  usernameInput: '[data-test="username-input"]',
+  signInPasswordInput: '[data-test="sign-in-password-input"]',
+  signInSignInButton: '[data-test="sign-in-sign-in-button"]',
+}
